530b7465b5750ec8ad00001d3962964633dd318e
filipkastovsky <devfilipkastovsky@gmail.com>
2020-09-21 21:54:44 +0200

    fix: 🐛 shit appear

--- src/index/App/shared/Appear.tsx
+++ src/index/App/shared/Appear.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentPropsWithoutRef, FC } from 'react';
+import { ComponentPropsWithoutRef, FC } from 'react';
 import { Appear as ShitAppear } from 'spectacle';
 type ShitProps = ComponentPropsWithoutRef<typeof ShitAppear>;
 export interface AppearProps
@@ -6,15 +6,4 @@ export interface AppearProps
         Pick<ShitProps, 'elementNum'> {}
 export type AppearType = FC<AppearProps>;
 
-const transitionObj = {
-    to: { opacity: 1 },
-    from: { opacity: 0 },
-};
-
-export const Appear: AppearType = ({ children, ...shitAppearProps }) => {
-    return (
-        <ShitAppear transitionEffect={transitionObj} {...shitAppearProps}>
-            {children}
-        </ShitAppear>
-    );
-};
+export const Appear: AppearType = ShitAppear as any;
